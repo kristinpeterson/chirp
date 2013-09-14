65a93f220a280399c3b75e27ccae37c928318a57
kristinpeterson <kristinpeterson@me.com>
2013-09-14 14:15:00 -0700

    Fixing heroku asset deployment error

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module SampleApp
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # added cause heroku said so: https://devcenter.heroku.com/articles/rails-asset-pipeline#troubleshooting
+    config.assets.initialize_on_precompile = false
   end
 end
