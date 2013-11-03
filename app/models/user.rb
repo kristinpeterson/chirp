5e1c5b2d455863ba6aa79cf714c1e80fd845e689
kristinpeterson <kristinpeterson@me.com>
2013-11-02 19:46:14 -0700

    Extra exercises from chapter 7

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,4 +20,5 @@ class User < ActiveRecord::Base
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
+  after_validation { self.errors.messages.delete(:password_digest) }
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -32,6 +32,13 @@ describe "User pages" do
       end
     end
 
+    describe "after submission" do
+      before { click_button submit }
+
+      it { should have_selector('title', text: 'Sign up') }
+      it { should have_content('error') }
+    end
+
     describe "with valid information" do
       before do
         fill_in "Name",         with: "Example User"
@@ -42,6 +49,14 @@ describe "User pages" do
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com') }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
   end
