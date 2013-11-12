67954c03d9131e7e8d41f02c51853bd27460b9fd
kristinpeterson <kristinpeterson@me.com>
2013-11-11 18:49:55 -0800

    removing secret_token...oops, now included in global gitignore

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-SampleApp::Application.config.secret_token = '3b9b3d1e5181b598b9e94a4507210376d8de32dff0b3ee17febcbaf3da9cd909f7e4705e7ec0538fede1591c31ea0c5fcd9c99ae78940399a1d700e894ca64ac'
