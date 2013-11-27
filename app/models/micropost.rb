0c58b6b314346f55d4afea8c4e4c40dd8316ad1b
kristinpeterson <kristinpeterson@me.com>
2013-11-26 21:00:44 -0800

    Add user following

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -6,4 +6,11 @@ class Micropost < ActiveRecord::Base
   default_scope order: 'microposts.created_at DESC'
   # alternatively *note* these are rails 3.2 compatible, different for rails 4.0+
   # default_scope order("created_at DESC")
+
+  def self.from_users_followed_by(user)
+    followed_user_ids = "SELECT followed_id FROM relationships
+                         WHERE follower_id = :user_id"
+    where("user_id IN (#{followed_user_ids}) OR user_id = :user_id",
+          user_id: user.id)
+  end
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -71,6 +71,16 @@ describe "Authentication" do
           before { visit users_path }
           it { should have_selector('title', text: 'Sign in') }
         end
+        
+        describe "visiting the following page" do
+          before { visit following_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
+        end
       end
 
       describe "when attempting to visit a protected page" do
@@ -80,7 +90,6 @@ describe "Authentication" do
         end
 
         describe "after signing in" do
-
           it "should render the desired protected page" do
             should have_title('Edit user')
           end
@@ -102,7 +111,6 @@ describe "Authentication" do
       end
 
       describe "in the Microposts controller" do
-
         describe "submitting to the create action" do
           before { post microposts_path }
           specify { response.should redirect_to(signin_path) }
@@ -110,6 +118,18 @@ describe "Authentication" do
 
         describe "submitting to the destroy action" do
           before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          specify { response.should redirect_to(signin_path) }
+        end
+      end
+
+      describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { response.should redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
           specify { response.should redirect_to(signin_path) }
         end
       end
